5350de1c177eabd92a0cce709746cc0b05259d5f
dimstav23 <dimstav23@gmail.com>
2024-08-05 06:19:00 -0700

    add numa binding and fix make command for gramine-tdx
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- UnixBench/run_unixbench_with_gramine.sh
+++ UnixBench/run_unixbench_with_gramine.sh
@@ -13,11 +13,13 @@ CURR_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 # Create results directory if it doesn't exist
 mkdir -p $RES_DIR
 
+BIND0="numactl --cpunodebind=0 --membind=0"
+
 # Source the benchmarks configuration
 source $THIS_DIR/unixbench_config.sh
 
 # Native execution
-UB_BINDIR=$THIS_DIR/pgms
+export UB_BINDIR=$THIS_DIR/pgms
 make clean && make
 
 # Loop over the benchmarks and execute them
@@ -28,7 +30,7 @@ for benchmark in "${!BENCHMARK_COMMANDS[@]}"; do
     for ((i = 1; i <= repeats; i++)); do
         result_file="$RES_DIR/native_${benchmark}_${i}.log"
         echo "Native --- Running benchmark: $benchmark with parameters: $command"
-        $PROG_DIR/$command > $result_file 2>&1
+        $BIND0 $PROG_DIR/$command > $result_file 2>&1
         echo "Saved results in $result_file"
     done
 done
@@ -47,7 +49,7 @@ for benchmark in "${!BENCHMARK_COMMANDS[@]}"; do
     for ((i = 1; i <= repeats; i++)); do
         result_file="$RES_DIR/gramine-sgx_${benchmark}_${i}.log"
         echo "Gramine SGX --- Running benchmark: $benchmark with parameters: $command"
-        gramine-sgx $command > $result_file 2>&1
+        $BIND0 gramine-sgx $command > $result_file 2>&1
         echo "Saved results in $result_file"
     done
 done
@@ -67,7 +69,7 @@ for benchmark in "${!BENCHMARK_COMMANDS[@]}"; do
     for ((i = 1; i <= repeats; i++)); do
         result_file="$RES_DIR/gramine-vm_${benchmark}_${i}.log"
         echo "Gramine VM --- Running benchmark: $benchmark with parameters: $command"
-        gramine-vm $command > $result_file 2>&1 & PID=$!
+        $BIND0 gramine-vm $command > $result_file 2>&1 & PID=$!
         
         sleep $TIMEOUT
         # Check if the process is still running
@@ -94,7 +96,7 @@ TIMEOUT=120
 export PATH=$GRAMINE_TDX_INSTALL_DIR/bin:$CURR_PATH
 export PYTHONPATH=$GRAMINE_TDX_INSTALL_DIR/lib/$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')/site-packages:$CURR_PYTHONPATH
 export PKG_CONFIG_PATH=$GRAMINE_TDX_INSTALL_DIR/lib/x86_64-linux-gnu/pkgconfig:$CURR_PKG_CONFIG_PATH
-make clean && make TDX=1
+make clean && make SGX=1
 
 # Loop over the benchmarks and execute them
 for benchmark in "${!BENCHMARK_COMMANDS[@]}"; do
@@ -104,7 +106,7 @@ for benchmark in "${!BENCHMARK_COMMANDS[@]}"; do
     for ((i = 1; i <= repeats; i++)); do
         result_file="$RES_DIR/gramine-tdx_${benchmark}_${i}.log"
         echo "Gramine TDX --- Running benchmark: $benchmark with parameters: $command"
-        gramine-tdx $command > $result_file 2>&1 & PID=$!
+        $BIND0 gramine-tdx $command > $result_file 2>&1 & PID=$!
         
         sleep $TIMEOUT
         # Check if the process is still running
